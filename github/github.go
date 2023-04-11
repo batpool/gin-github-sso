48523de4781e6b6373afdf64514a5455f12f3e8e
batpool <satyabrata.7059@icloud.com>
2023-04-11 21:29:12 +0530

    html

--- github/github.go
+++ github/github.go
@@ -80,7 +80,7 @@ func LoginHandler(ctx *gin.Context) {
 		<meta charset="UTF-8">
 		<meta http-equiv="X-UA-Compatible" content="IE=edge">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
-		<title>Document</title>
+		<title>SSO Login</title>
 		<style>
 			@import url('https://fonts.googleapis.com/css?family=Poppins');
 
